eccad7b6d3b742e125445a8f53631f7f7c0a9bde
Santiago Tovar <satovarr@unal.edu.co>
2022-10-25 09:45:08 -0500

    Sprint 3.2

--- routes/about.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-about = APIRouter()
-
-@about.get('/about')
-def get_about():
-  return "about"
--- templates/templates.py
+++
@@ -1,4 +0,0 @@
-# Need to be imported in all routes, by now.
-from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="templates")
