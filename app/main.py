4a930eb1ad405b89f956c5a4c22fdf933f6c7f02
satovarr <satovarr@unal.edu.co>
2022-11-16 20:48:06 -0500

    CORS added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import tutoring, user
 from .sql import models
 from .sql.database import engine
@@ -7,7 +8,18 @@ models.Base.metadata.create_all(bind=engine, checkfirst=True)
 
 app = FastAPI()
 
+origins = [
+    "https://unitutor-f0c21.web.app", 
+    "https://unitutor-f0c21.firebaseapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tutoring.router)
 app.include_router(user.router)
-    
