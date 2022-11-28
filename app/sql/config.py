3baae893b69a23f5b3d61b6d91d6794fe1dbf62c
santiago tovar <satovarr@unal.edu.co>
2022-11-28 09:37:44 -0500

    cambio de contraseña db

--- app/sql/config.py
+++ app/sql/config.py
@@ -1,8 +1,8 @@
 config = {
+    "DB_DIALECT": "mysql+mysqlconnector",
     "DB_USER": "root",
-    "DB_PASS": "DUzUreSEa6hDML52dOoG",
+    "DB_PASS": "yKcvP7Ic5Zwu97rZ4wli",
     "DB_HOST": "containers-us-west-95.railway.app",
     "DB_PORT": "5735",
     "DB_SCHEMA": "railway",
-    "DB_DIALECT": "mysql+mysqlconnector"
 }
