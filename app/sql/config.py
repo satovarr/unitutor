fc4c14f72b09263bffaa9a6947b343390ee0f27b
santiago tovar <satovarr@unal.edu.co>
2022-11-28 09:51:41 -0500

    nuevas credenciales database

--- app/sql/config.py
+++ app/sql/config.py
@@ -2,7 +2,7 @@ config = {
     "DB_USER": "root",
     "DB_DIALECT": "mysql+mysqlconnector",
     "DB_PASS": "yKcvP7Ic5Zwu97rZ4wli",
-    "DB_HOST": "containers-us-west-95.railway.app",
-    "DB_PORT": "5735",
+    "DB_HOST": "containers-us-west-37.railway.app",
+    "DB_PORT": "5631",
     "DB_SCHEMA": "railway",
 }
