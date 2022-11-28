b9b068b99947d2f85e5532607ee637c8eb153c24
santiago tovar <satovarr@unal.edu.co>
2022-11-28 09:45:04 -0500

    intento de deploy

--- app/sql/config.py
+++ app/sql/config.py
@@ -1,6 +1,6 @@
 config = {
-    "DB_DIALECT": "mysql+mysqlconnector",
     "DB_USER": "root",
+    "DB_DIALECT": "mysql+mysqlconnector",
     "DB_PASS": "yKcvP7Ic5Zwu97rZ4wli",
     "DB_HOST": "containers-us-west-95.railway.app",
     "DB_PORT": "5735",
