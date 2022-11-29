8dacc1dfcf8efffeff80d1bc17020973419c1ca9
slondonoq <slondonoq@unal.edu.co>
2022-11-28 21:18:55 -0500

    Not found page done

--- client/src/routes/NotFound.jsx
+++ client/src/routes/NotFound.jsx
@@ -1,10 +1,16 @@
 import logo from '../imgs/UniTutor_logo.svg'
+import brokenPage from '../imgs/brokenpage.svg'
+import '../styles/notFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <img alt='unitutor' src={logo}/>
-            <p><span>404</span> page not found</p>
+        <div className="notfound">
+            <div>
+                <img alt='unitutor' src={logo} className="logo"/>
+                <p className="heading">Ooops, 404! Tenemos un 404! </p>
+                <p>La URL ingresada no se encontró en el servidor :(</p>
+            </div>
+            <img alt='error' src={brokenPage} className="brokenpage"/>
         </div>
     )
 }
